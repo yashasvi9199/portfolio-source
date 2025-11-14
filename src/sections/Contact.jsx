27ef90756f993746d31de3750a7c2b95272243f4
Yash Haldiya <yashasvi9199@gmail.com>
2025-11-14 16:13:30 +0530

    fix : import issues

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
 import '../styles/sections/Contact.css';
-import config from '../config'; // ADD this import
 
 const Contact = () => {
     const contactRef = useRef(null);
