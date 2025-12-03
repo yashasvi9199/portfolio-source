1a41a29afbce324424fa714dc3ade2be59742b1c
Yash Haldiya <yashasvi9199@gmail.com>
2025-12-03 18:06:42 +0530

    fix: cookies domain blocked issue

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -2,6 +2,8 @@ export const trackPageView = (path) => {
     if (window.gtag) {
         window.gtag('config', 'G-STRJLDTGXC', {
             page_path: path,
+            cookie_domain: window.location.hostname,
+            cookie_flags: 'SameSite=None;Secure'
         });
     }
 };
